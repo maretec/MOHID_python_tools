c8830bc717d258458a5aeb9a56e8523f92170907
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2018-11-14 16:57:01 +0000

    Particles written in 3D and with a data field, now pointed at Origin ID

--- HDF5toNetCDF/MOHIDLagHDFtoXYZ.py
+++ HDF5toNetCDF/MOHIDLagHDFtoXYZ.py
@@ -8,25 +8,29 @@
 import h5py
 import numpy as np
 
-# Change your filename here
-#filename = 'C:\\Users\\administrator\\Documents\\GitHub\\HDFtoXYZ\\Lagrangian_1.hdf5'
 def MOHIDLagHdf5toNetcdf(filename,in_t=0,outdir=''):
     f = h5py.File(filename,'r')
     
     TimestepsX = f['Results']['Group_1']['Data_1D']['Longitude'].keys()
     TimestepsY = f['Results']['Group_1']['Data_1D']['Latitude'].keys()
     TimestepsZ = f['Results']['Group_1']['Data_1D']['Z Pos'].keys()
+    TimestepsOid = f['Results']['Group_1']['Data_1D']['Origin ID'].keys()
     
     t=in_t
     for time in range(0,len(TimestepsX)):
         
         X=f['Results']['Group_1']['Data_1D']['Longitude'][TimestepsX[time]][:]
         Y=f['Results']['Group_1']['Data_1D']['Latitude'][TimestepsY[time]][:]
-        #Z=f['Results']['Group_1']['Data_1D']['Z Pos'][TimestepsZ[time]][:]
-        Z=np.zeros(len(X))
+        Z=f['Results']['Group_1']['Data_1D']['Z Pos'][TimestepsZ[time]][:]
+        OiD=f['Results']['Group_1']['Data_1D']['Origin ID'][TimestepsOid[time]][:]
+        #Z=np.zeros(len(X))
         
-        XYZ=np.stack((X,Y,Z)).transpose()
-        np.savetxt(outdir+'XYZ'+str(t)+'.csv',XYZ,delimiter=';')
+        XYZ=np.stack((X,Y,Z,OiD)).transpose()
+        np.savetxt(outdir+'XYZ'+str(t)+'.csv',XYZ,delimiter=',')
         t=t+1
         
     return t
+
+# Change your filename hereD:\PV_data\PCOMS_Lagrangian\temp
+filename = 'D:\\PV_data\\PCOMS_Lagrangian\\temp\\Lagrangian.hdf5'
+t = MOHIDLagHdf5toNetcdf(filename)
