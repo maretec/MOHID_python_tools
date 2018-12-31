734965e84136102003153a3cca1394f6499a7cca
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2018-12-31 23:01:46 +0000

    Searching recursively in all subdirectories and invoking the MXDMF maker class for each found file

--- MXDMF/src/MXDMF_run.py
+++ MXDMF/src/MXDMF_run.py
@@ -2,17 +2,27 @@
 
 import os
 import sys
-sys.path.append('../../Common')
 
-import os_dir as os_dir
+sys.path.append('../../Common')
+import os_dir
+
+import MXDMF_maker
 
 ################################################################################
 
-#MOHID output hdf file directory
+#file directory where MOHID hdf5 files are, or subdirectories with them 
 basepath = os.path.dirname(__file__)
 datadir = os.path.abspath(os.path.join(basepath, "..", "TestFiles"))
-print '--> Main working directory is', datadir 
-#subdirs = os_dir.get_immediate_subdirectories(datadir)
-#print subdirs
-files = os_dir.get_contained_files(datadir,'.hdf5')
-print files
+print '--> Main working directory is', datadir
+#files may be in sub directories
+subdirs = os_dir.get_immediate_subdirectories(datadir)
+#if subdirs is empty then just point to the main directory, as a list
+if subdirs == []:
+    subdirs = [datadir]
+
+#go through all subdirs 
+for subdir in subdirs:
+    hdf5files = os_dir.get_contained_files(subdir,'.hdf5')
+    print hdf5files
+    for hdf5file in hdf5files:
+        writexdmf = MXDMF_maker.MXDMFmaker(hdf5file,subdir)
