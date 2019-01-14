4d15c1efd04c4ea9070efb7b2fab688dba9d7743
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-01-14 18:53:47 +0000

    Full encoding/decoding of MOHID dates
    using float to represent days since a given reference date. retuns time stamps, strings and dates

--- Common/MDateTime.py
+++ Common/MDateTime.py
@@ -4,16 +4,38 @@ from datetime import datetime, timedelta
 
 ## this encondes/decodes the MOHID date/time array to/from a float of total time since a reference date, in days 
 
-def BaseDateTime():
-    return datetime(1950, 1, 1, 0, 0, 0)
+# Public API
 
-def getTimeStampFromDate(MohidDate):
-    delta = datetime(MohidDate[0], MohidDate[1], MohidDate[2], MohidDate[3], MohidDate[4], MohidDate[5]) - BaseDateTime()
+def getTimeStampFromMOHIDDate(MohidDate):
+    delta = datetime(int(MohidDate[0]), int(MohidDate[1]), int(MohidDate[2]), int(MohidDate[3]), int(MohidDate[4]), int(MohidDate[5])) - BaseDateTime()
     timeStamp = delta.total_seconds()/timedelta(days=1).total_seconds()
     return timeStamp
 
 def getMOHIDDateFromTimeStamp(timeStamp):
-    delta = timedelta(seconds=timeStamp*timedelta (days=1).total_seconds())
-    MD = BaseDateTime() + delta
+    MD = getDateTimeFromTimeStamp(timeStamp)
     MohidDate = [MD.year, MD.month, MD.day, MD.hour, MD.minute, MD.second]
     return MohidDate
+
+def getDateStringFromTimeStamp(timeStamp):
+    return getDateTimeFromTimeStamp(timeStamp).strftime("%Y-%m-%d %H:%M:%S")
+
+def getDateStringFromMOHIDDate(MohidDate):
+    timeStamp = getTimeStampFromMOHIDDate(MohidDate)
+    return getDateTimeFromTimeStamp(timeStamp).strftime("%Y-%m-%d %H:%M:%S")
+
+
+###private functions
+
+def BaseDateTime():
+    return datetime(1950, 1, 1, 0, 0, 0)
+
+def getDateTimeFromTimeStamp(timeStamp):
+    delta = timedelta(seconds=timeStamp*timedelta (days=1).total_seconds())
+    return BaseDateTime() + delta
+
+#API examples
+#MOHIDate = [2000, 8, 19, 1, 1, 37]
+#print getTimeStampFromMOHIDDate(MOHIDate)
+#print getMOHIDDateFromTimeStamp(getTimeStampFromMOHIDDate(MOHIDate))
+#print getDateStringFromTimeStamp(getTimeStampFromMOHIDDate(MOHIDate))
+#print getDateStringFromMOHIDDate(MOHIDate)
