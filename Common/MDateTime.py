4720f66088fc500bf2d530826a3492e86030f699
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-01-14 12:15:53 +0000

    encondes/decodes the MOHID date/time array to/from a float of total time since a reference date, in days

--- Common/MDateTime.py
+++ Common/MDateTime.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from datetime import date
+from datetime import datetime, timedelta
+
+## this encondes/decodes the MOHID date/time array to/from a float of total time since a reference date, in days 
+
+def BaseDateTime():
+    return datetime(1950, 1, 1, 0, 0, 0)
 
 def getTimeStampFromDate(MohidDate):
-    d0 = date(1950, 1, 1)
-    d1 = date(MohidDate[0], MohidDate[1], MohidDate[2])
-    delta = d1 - d0
-    print delta.days
-    
-    
-getTimeStampFromDate([2000, 8, 19])
+    delta = datetime(MohidDate[0], MohidDate[1], MohidDate[2], MohidDate[3], MohidDate[4], MohidDate[5]) - BaseDateTime()
+    timeStamp = delta.total_seconds()/timedelta(days=1).total_seconds()
+    return timeStamp
+
+def getMOHIDDateFromTimeStamp(timeStamp):
+    delta = timedelta(seconds=timeStamp*timedelta (days=1).total_seconds())
+    MD = BaseDateTime() + delta
+    MohidDate = [MD.year, MD.month, MD.day, MD.hour, MD.minute, MD.second]
+    return MohidDate
