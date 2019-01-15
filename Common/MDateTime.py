5788d27efd1239a831fd547c5f2ffc9574271508
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-01-15 12:34:04 +0000

    Licence and description headers updated

--- Common/MDateTime.py
+++ Common/MDateTime.py
@@ -1,8 +1,46 @@
 # -*- coding: utf-8 -*-
 
+#    !------------------------------------------------------------------------------
+#    !        IST/MARETEC, Water Modelling Group, Mohid modelling system
+#    !------------------------------------------------------------------------------
+#    !
+#    ! TITLE         : MDateTime
+#    ! PROJECT       : Mohid python tools
+#    ! MODULE        : background
+#    ! URL           : http://www.mohid.com
+#    ! AFFILIATION   : IST/MARETEC, Marine Modelling Group
+#    ! DATE          : January 2019
+#    ! REVISION      : Canelas 0.1
+#    !> @author
+#    !> Ricardo Birjukovs Canelas
+#    !
+#    ! DESCRIPTION:
+#    !this encondes/decodes the MOHID date/time array to/from a float of total time since a reference date, in days 
+#    !------------------------------------------------------------------------------
+#    
+#    MIT License
+#    
+#    Copyright (c) 2018 RBCanelas
+#    
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#    
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#    
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+    
 from datetime import datetime, timedelta
-
-## this encondes/decodes the MOHID date/time array to/from a float of total time since a reference date, in days 
 
 # Public API
 
--- MXDMF/src/MXDMF_maker.py
+++ MXDMF/src/MXDMF_maker.py
@@ -1,9 +1,47 @@
 # -*- coding: utf-8 -*-
 
-#this class is resposible for, given a MOHID HDF5 file, reading it's fields
-#and writting a correspoing XDMF file
-#it is composed of a number of objects from other classes, that do most of the 
-#actual work. This is the pupeteer...
+#    !------------------------------------------------------------------------------
+#    !        IST/MARETEC, Water Modelling Group, Mohid modelling system
+#    !------------------------------------------------------------------------------
+#    !
+#    ! TITLE         : MXDMF_run
+#    ! PROJECT       : Mohid python tools
+#    ! MODULE        : background
+#    ! URL           : http://www.mohid.com
+#    ! AFFILIATION   : IST/MARETEC, Marine Modelling Group
+#    ! DATE          : January 2019
+#    ! REVISION      : Canelas 0.1
+#    !> @author
+#    !> Ricardo Birjukovs Canelas
+#    !
+#    ! DESCRIPTION:
+#    !this class is resposible for, given a MOHID HDF5 file, reading it's fields
+#    !and writting a correspoing XDMF file
+#    !it is composed of a number of objects from other classes, that do most of the 
+#    !actual work. This is a pupeteer.
+#    !------------------------------------------------------------------------------
+#    
+#    MIT License
+#    
+#    Copyright (c) 2018 RBCanelas
+#    
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#    
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#    
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
 
 import sys
 sys.path.append('../../Common')
