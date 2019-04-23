c2991f794b50428bb7c59d6bd290217472b9701f
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-04-23 15:09:49 +0100

    Creation of Paraview scripts repo directory. Date-time annotation introduced

---
+++ ParaviewScripts/DateTimeAnnotation.py
@@ -0,0 +1,38 @@
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+# find source
+source = GetActiveSource()
+# create a new 'Programmable Filter'
+programmableFilter1 = ProgrammableFilter(Input=source)
+programmableFilter1.RequestInformationScript = ''
+programmableFilter1.RequestUpdateExtentScript = ''
+programmableFilter1.PythonPath = ""
+# Properties modified on programmableFilter1
+programmableFilter1.OutputDataSetType = 'vtkTable'
+
+programmableFilter1.Script = '\
+import sys\n\
+sys.path.append("C:\Users\RBC_workhorse\Documents\GitHub\MOHID_python_tools\Common")\n\
+import MDateTime as mdate\n\
+t = inputs[0].GetInformation().Get(vtk.vtkDataObject.DATA_TIME_STEP())\n\
+date_time = mdate.getDateStringFromTimeStamp(t)\n\
+outputarray = vtk.vtkStringArray()\n\
+outputarray.SetName("datetime")\n\
+outputarray.SetNumberOfTuples(1)\n\
+outputarray.SetValue(0, "{}".format(date_time))\n\
+print("{} -> {}".format(t, date_time))\n\
+output.RowData.AddArray(outputarray)'
+
+# find view
+renderView1 = GetActiveView()
+# create a new 'Python Annotation'
+pythonAnnotation1 = PythonAnnotation(Input=programmableFilter1)
+# Properties modified on pythonAnnotation1
+pythonAnnotation1.ArrayAssociation = 'Row Data'
+pythonAnnotation1.Expression ='"{}".format(input.RowData["datetime"].GetValue(0))'
+# show data in view
+pythonAnnotation1Display = Show(pythonAnnotation1, renderView1)
+# update the view to ensure updated data information
+renderView1.Update()
---
+++ ParaviewScripts/README.md
@@ -0,0 +1,10 @@
+# Paraview scripts
+
+Paraview python scripts to automate/simplify data presentation.
+
+	DateTimeAnnotation - Converts the time-step data into a date-time format and prints to screen. 
+						This uses the time-stamp used in MXDMF, that represents days since a reference date. Encoding/decoding is implemented in /Common/MDateTime.py
+						Development in progress.
+						1 - Change the hardcoded path of the /Common directory of the python tools repo to your system
+						2 - Load as a macro and run.
+
