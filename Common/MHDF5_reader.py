47125f4bd84d55995dd5010a6d8cd47f0d6cd417
RBCanelas <34510178+RBCanelas@users.noreply.github.com>
2019-04-29 15:32:44 +0100

    Update MHDF5_reader.py

--- Common/MHDF5_reader.py
+++ Common/MHDF5_reader.py
@@ -77,7 +77,7 @@ class MHDF5Reader:
             if 'water level' in list(self.f['Results'].keys()):
                 self.fileType = 'Hydrodynamic'
                 #Because 2D fiels are mixed with 3D fields
-                exclusions = ['Error','TidePotential','water column','water level']                
+                exclusions = ['Error','TidePotential','water column','water level','VolumeCreated']                
                 if self.getGeoDims() == 2:
                     self.fileType = 'Hydrodynamic2D'
                     exclusions = []
@@ -120,7 +120,7 @@ class MHDF5Reader:
                 self.fVars = list(self.f['Results']['Group_1']['Data_1D'].keys())
                 #Because conventions are not followed (name of the variable 
                 #is not the name of the field, mixing diferent dimenisionalities on the same group,...)
-                exclusions = ['X Pos','Y Pos','Z Pos','Latitude average','Longitude average', 'googlemaps_x_average', 'googlemaps_y_average']
+                exclusions = ['X Pos','Y Pos','Z Pos','Latitude average','Longitude average', 'googlemaps_x_average', 'googlemaps_y_average','VolumeCreated']
                 for exc in exclusions:
                     if exc in self.fVars:
                         self.fVars.remove(exc)
